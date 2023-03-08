abffff26448ab16a1315acab89722c578c8f372c
Kartavya Sharma <kartavya@berkeley.edu>
2023-03-08 10:36:09 -0800

    More refactor

--- gc_content/script.py
+++ gc_content/script.py
@@ -12,22 +12,10 @@ with open("rosalind_gc.txt") as file:
             curr_string += line.rstrip()
     sequences.append(curr_string)
 
-# breakpoint()
-
-out_len = len(lables)
-gc_content = [0 for _ in range(out_len)]
-for idx in range(out_len):
-    gc_count = 0
-    for char in sequences[idx]:
-        if char == "G" or char == "C":
-            gc_count += 1
-    gc_content[idx] = (lables[idx], (gc_count / len(sequences[idx]))*100)
-
-# max_label, max_percentage = "", 0
-# for idx in range(out_len):
-#     if gc_content[idx] > max_percentage:
-#         max_label = lables[idx]
-#         max_percentage = gc_content[idx]
+gc_content = []
+for idx in range(len(lables)):
+    gc_count = sum([1 for char in sequences[idx] if char == "G" or char == "C"])
+    gc_content.append((lables[idx], (gc_count / len(sequences[idx]))*100))
 
 gc_content.sort(key=lambda x: x[1])
 
